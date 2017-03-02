e43ac19ef2dc1b0fb5b9490f865adc4772eda22b
Norbert Nemeth <nnorb93@gmail.com>
2017-03-02 09:18:43 +0100

    Refactor Router

--- src/app/Router.ts
+++ src/app/Router.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import * as request from 'request';
 
-var path = require('path')
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var express = require('express');
+import path = require('path');
+import logger = require('morgan');
+import bodyParser = require('body-parser');
+import cookieParser = require('cookie-parser');
+import express = require('express');
 
 export default function Router() {
   return {
@@ -25,50 +25,50 @@ export default function Router() {
 
 
       if (app.get('env') === 'development') {
-        app.use(function(err: any, req: Request, res: Response, next: Function) {
+        app.use((err: any, req: Request, res: Response, next: Function) => {
           res.status(err.status || 500);
           res.render('error', {
             message: err.message,
-            error: err
+            error: err,
           });
         });
       }
 
-      app.use(function(err: any, req: Request, res: Response, next: Function) {
+      app.use((err: any, req: Request, res: Response, next: Function) => {
         res.status(err.status || 500);
         res.render('error', {
           message: err.message,
-          error: {}
+          error: {},
         });
       });
 
-      router.get('/', function (req: Request, res: Response, next: Function) {
+      router.get('/', (req: Request, res: Response, next: Function) => {
         request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers`, timeout: 4000 },
           (err, catRes, categories) => {
             request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers`, timeout: 4000  },
-              (err, flowerRes, flowerList) => {
+              (error, flowerRes, flowerList) => {
                 let data = { categories: JSON.parse(categories), flowerList: JSON.parse(flowerList) }
                 res.render('index', data)
               })
           })
       });
 
-      router.get('/checkout', function (req: Request, res: Response, next: Function) {
+      router.get('/checkout', (req: Request, res: Response, next: Function) => {
         request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers`, timeout: 4000  },
           (err, catRes, categories) => {
             request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers`, timeout: 4000  },
-              (err, flowerRes, flowerList) => {
+              (error, flowerRes, flowerList) => {
                 let data = { categories: JSON.parse(categories), flowerList: JSON.parse(flowerList), checkout:true }
                 res.render('index', data)
               })
           })
       });
 
-      router.get('/category/:catName', function (req: Request, res: Response, next: Function) {
+      router.get('/category/:catName', (req: Request, res: Response, next: Function) => {
         request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers`, timeout: 4000  },
           (err, catRes, categories) => {
             request.get({ url: `http://${endpoints.getServiceAddress(`localhost:3003`)}/data/flowers/` + req.params['catName'], timeout: 4000  },
-              (err, flowerRes, flowerList) => {
+              (error, flowerRes, flowerList) => {
                 if (err) return console.log(err), res.status(500).json(err)
                 let data = { categories: JSON.parse(categories), flowerList: JSON.parse(flowerList) }
                 const activeCategory = data.categories.find(c => c.Name === req.params['catName']);
@@ -77,17 +77,14 @@ export default function Router() {
               })
           })
       });
-      router.get('/asd', (req, res) => {res.send('OK')})
-
 
       app.use('/', router);
 
       app.use((req: Request, res: Response, next: Function) => {
-        var err: any = new Error('Not Found');
+        let err: any = new Error('Not Found');
         err.status = 404;
         next(err);
       });
-
     }
   }
 }
